386086f48f820a07bb008dc80cc05f7fcb1fb0d1
Sangg <ngocsangyem@gmail.com>
2022-04-15 21:31:19 +0700

    :package:  add quasar framework

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue({
+			template: { transformAssetUrls },
+		}),
+		quasar({
+			sassVariables: 'src/styles/quasar-variables.scss',
+		}),
+	],
 	resolve: {
 		alias: {
 			'@/': resolve(__dirname, './src'),
