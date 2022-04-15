86ea37303f1740abe643892ffbbfce6474329f0d
Sangg <ngocsangyem@gmail.com>
2022-04-15 23:48:19 +0700

    :sparkles:  add icons

---
+++ src/common/icons/DownArrowIcon.vue
@@ -0,0 +1,29 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="20"
+		height="20"
+		viewBox="0 0 20 20"
+		class="down-arrow-icon"
+	>
+		<g class="a" transform="translate(-0.001)">
+			<g transform="translate(0.001)">
+				<path
+					class="b"
+					d="M10,0A10,10,0,1,0,20,10,10,10,0,0,0,10,0Zm4.512,8.776-3.736,3.736a.914.914,0,0,1-.125.158.951.951,0,0,1-1.479-.158L5.42,8.76A.933.933,0,1,1,6.74,7.44l3.235,3.234,3.217-3.218a.934.934,0,0,1,1.321,1.32Z"
+					transform="translate(-0.001)"
+				/>
+			</g>
+		</g>
+	</svg>
+</template>
+<style lang="scss" scoped>
+.down-arrow-icon {
+	.a {
+		opacity: 0.15;
+	}
+	.b {
+		fill: #325baf;
+	}
+}
+</style>
---
+++ src/common/icons/index.ts
@@ -0,0 +1,51 @@
+import AspireIcon from './AspireIcon.vue';
+import AspireLogo from './AspireLogo.vue';
+import AspireLogoWhite from './AspireLogoWhite.vue';
+import PaymentIcon from './PaymentIcon.vue';
+import DebitCardIcon from './DebitCardIcon.vue';
+import CreditCardIcon from './CreditCardIcon.vue';
+import FreezeIcon from './FreezeIcon.vue';
+import LimitIcon from './LimitIcon.vue';
+import AccountIcon from './AccountIcon.vue';
+import UserIcon from './AccountIcon.vue';
+import AddIcon from './AddIcon.vue';
+import CardSmallIcon from './CardSmallIcon.vue';
+import DeactivateCardIcon from './DeactivateCardIcon.vue';
+import DownArrowIcon from './DownArrowIcon.vue';
+import FileStorageIcon from './FileStorageIcon.vue';
+import FlightIcon from './FlightIcon.vue';
+import GPayIcon from './GPayIcon.vue';
+import MegaPhoneIcon from './MegaPhoneIcon.vue';
+import NextArrowIcon from './NextArrowIcon.vue';
+import PickupCarIcon from './PickupCarIcon.vue';
+import RemovedEyeIcon from './RemovedEyeIcon.vue';
+import ReplaceCardIcon from './ReplaceCardIcon.vue';
+import TransactionIcon from './TransactionIcon.vue';
+import VisaLogo from './VisaLogo.vue';
+
+export {
+	AspireIcon,
+	AspireLogo,
+	AspireLogoWhite,
+	PaymentIcon,
+	DebitCardIcon,
+	CreditCardIcon,
+	FreezeIcon,
+	LimitIcon,
+	AccountIcon,
+	UserIcon,
+	AddIcon,
+	CardSmallIcon,
+	DeactivateCardIcon,
+	DownArrowIcon,
+	FileStorageIcon,
+	FlightIcon,
+	GPayIcon,
+	MegaPhoneIcon,
+	NextArrowIcon,
+	PickupCarIcon,
+	RemovedEyeIcon,
+	ReplaceCardIcon,
+	TransactionIcon,
+	VisaLogo,
+};
