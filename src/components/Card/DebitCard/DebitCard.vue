923ef24106605513e3ab0919e3410c2970f53c20
Sangg <ngocsangyem@gmail.com>
2022-04-16 14:43:36 +0700

    :lipstick:  add base styles

---
+++ src/components/Card/DebitCard/DebitCard.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="debit-card-container">
+		<button class="debit-card-show">
+			<removed-eye-icon></removed-eye-icon>
+			<span>Show card icon</span>
+		</button>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { RemovedEyeIcon } from "@/common/icons";
+</script>
+
+<style lang="scss" scoped>
+
+</style>
