d3200509eece913726d291e0490ac206c60dfa96
Sangg <ngocsangyem@gmail.com>
2022-04-16 16:57:53 +0700

    :sparkles: sidenav done

---
+++ src/components/Navigation/NavMenu/NavMenu.vue
@@ -0,0 +1,85 @@
+<template>
+	<nav class="shadow-menu md:shadow-none">
+		<ul class="flex md:flex-col justify-around">
+			<li v-for="menuItem in menu" :key="menuItem.name">
+				<a
+					:href="menuItem.path"
+					:class="`text-tiny py-3 md:py-0 md:text-xl flex flex-col md:flex-row items-center active:text-primary ${isActive(
+						menuItem.isActive
+					)}`"
+					:title="menuItem.name"
+				>
+					<span class="inline-block mb-1 md:mb-0">
+						<component :is="menuItem.icon"></component>
+					</span>
+					<span class="md:ml-4 inline-block">{{ menuItem.name }}</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script setup lang="ts">
+import { markRaw } from 'vue';
+import AspireIcon from '@/common/icons/AspireIcon.vue';
+import DebitCardIcon from '@/common/icons/DebitCardIcon.vue';
+import PaymentIcon from '@/common/icons/PaymentIcon.vue';
+import CreditCardIcon from '@/common/icons/CreditCardIcon.vue';
+import UserIcon from '@/common/icons/UserIcon.vue';
+
+/*
+ * Todo
+ * dynamic active class by route
+ */
+const menu = [
+	{
+		name: 'Home',
+		path: '/',
+		icon: markRaw(AspireIcon),
+		isActive: false,
+	},
+	{
+		name: 'Cards',
+		path: '/',
+		icon: markRaw(DebitCardIcon),
+		isActive: true,
+	},
+	{
+		name: 'Payments',
+		path: '/',
+		icon: markRaw(PaymentIcon),
+		isActive: false,
+	},
+	{
+		name: 'Credit',
+		path: '/',
+		icon: markRaw(CreditCardIcon),
+		isActive: false,
+	},
+	{
+		name: 'Settings',
+		path: '/',
+		icon: markRaw(UserIcon),
+		isActive: false,
+	},
+];
+const isActive = (active: boolean) => {
+	return active
+		? 'text-primary'
+		: 'text-gray-acadia md:text-white hover:md:text-primary md:transition';
+};
+</script>
+
+<style lang="scss" scoped>
+.is-active {
+	@apply text-primary;
+}
+
+li {
+	@include breakpoint(md) {
+		+ li {
+			margin-top: 60px;
+		}
+	}
+}
+</style>
---
+++ src/components/Navigation/SideNav/SideNav.vue
@@ -0,0 +1,37 @@
+<template>
+	<aside
+		class="side-nav md:bg-secondary w-full md:w-340 md:p-12 fixed md:top-0 left-0 bottom-0 md:h-full"
+	>
+		<header class="mb-20 hidden md:block">
+			<a
+				href="https://aspireapp.com/"
+				class="aspire-logo"
+				target="_blank"
+				rel="noopener"
+			>
+				<span class="inline-block">
+					<aspire-logo></aspire-logo>
+				</span>
+			</a>
+			<p class="opacity-30 text-white text-lg">
+				Trusted way of banking for 3,000+ SMEs and startups in Singapore
+			</p>
+		</header>
+		<nav-menu></nav-menu>
+	</aside>
+</template>
+
+<script setup lang="ts">
+import AspireLogo from '@/common/icons/AspireLogo.vue';
+import NavMenu from '../NavMenu/NavMenu.vue';
+</script>
+
+<style lang="scss" scoped>
+.aspire-logo {
+	span {
+		&:first-child {
+			margin-bottom: 19px;
+		}
+	}
+}
+</style>
