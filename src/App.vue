f13fdcf7d137248cc7c8bbdf61e74c27871748e4
Sangg <ngocsangyem@gmail.com>
2022-04-16 17:29:15 +0700

    :art:  update layout

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@ import SideNav from '@/components/Navigation/SideNav/SideNav.vue';
 </script>
 
 <template>
-	<side-nav></side-nav>
-	<router-view></router-view>
+	<div class="md:flex">
+		<side-nav></side-nav>
+		<router-view></router-view>
+	</div>
 </template>
 
 <style lang="scss">
