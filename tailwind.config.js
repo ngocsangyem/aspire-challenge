ed54b920c82f08bd459ff83c68ac939b9b55e546
Sangg <ngocsangyem@gmail.com>
2022-04-16 16:57:34 +0700

    :lipstick:  config colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,59 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: 'var(--color-primary)',
+				secondary: 'var(--color-secondary)',
+				black: 'var(--color-black)',
+				white: 'var(--color-white)',
+				blue: 'var(--color-blue)',
+				'blue-light': 'var(--color-blue-light)',
+				'blue-lighter': 'var(--color-blue-lighter)',
+				'blue-dark': 'var(--color-blue-dark)',
+				'gray-acadia': 'var(--color-gray-acadia)',
+			},
+			width: {
+				340: '340px',
+			},
+			fontSize: {
+				h1: 'var(--text-xxl, 2rem)',
+				h2: 'var(--text-xl, 1.75rem)',
+				h3: 'var(--text-lg, 1.375rem)',
+				h4: 'var(--text-md, 1.125rem)',
+				base: 'var(--text-base-size, 1rem)',
+				tiny: ['var(--text-tiny-size, 0.5rem)', '13px'],
+				xs: ['var(--text-xs-size, 0.75rem)', '20px'],
+				sm: ['var(--text-sm-size, 0.875rem)', '18px'],
+				md: ['var(--text-md-size, 1rem)', '19px'],
+				lg: ['var(--text-lg-size, 1.125rem)', '20px'],
+				xl: ['var(--text-xl-size, 1.25rem)', '32px'],
+				'2xl': ['var(--text-2xl-size, 1.5rem)', '20px'],
+				'3xl': ['var(--text-3xl-size, 1.875rem)', '16px'],
+			},
+			boxShadow: {
+				'menu': '0px 0px 8px rgba(0, 0, 0, 0.04)',
+			},
+		},
+		screens: {
+			xs: '32rem',
+			// => @media (min-width: 512px) { ... }
+
+			sm: '48rem',
+			// => @media (min-width: 768px) { ... }
+
+			md: '64rem',
+			// => @media (min-width: 1024px) { ... }
+
+			lg: '80rem',
+			// => @media (min-width: 1280px) { ... }
+
+			xl: '90rem',
+			// => @media (min-width: 1440px) { ... }
+
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
 	},
 	plugins: [],
 };
