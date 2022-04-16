be8d7d0ac44b664d3cbc8b5e455008efd340ae81
Sangg <ngocsangyem@gmail.com>
2022-04-16 18:17:18 +0700

    :sparkles:  add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import { router } from './routes';
+import { createPinia } from 'pinia';
 import './index.css';
 import App from './App.vue';
-const app = createApp(App);
 
+const app = createApp(App);
+const pinia = createPinia()
+
+app.use(pinia);
 app.use(router)
 app.mount('#app');
 
